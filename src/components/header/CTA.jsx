629b26aa8bada78305f5d1ad4ee07172783a9da4
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-05-18 12:32:11 +0600

    Saving to GitHub

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from '../../assets/Inzamum-Ul-Haque.pdf'
 
 const CTA = () => {
   return (
-    <div>
+    <div className='cta'>
         <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
