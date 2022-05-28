e4a955136a81c949a137a672046be8bcabf2398d
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-05-28 20:54:01 +0600

    Saving to GitHub

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./testimonials.css";
-import AVATR1 from "../../assets/avatar1.jpg";
-import AVATR2 from "../../assets/avatar2.jpg";
-import AVATR3 from "../../assets/avatar3.jpg";
-import AVATR4 from "../../assets/avatar4.jpg";
+import AVATR1 from "../../assets/coderstrust.jpg";
+import AVATR2 from "../../assets/css.jpg";
+import AVATR3 from "../../assets/google_it_support.jpg";
+import AVATR4 from "../../assets/html.jpg";
+import AVATR5 from "../../assets/excel.jpg";
+import AVATR6 from "../../assets/resume.jpg";
 
 // import Swiper core and required modules
 import { Pagination } from "swiper";
@@ -17,25 +19,37 @@ import "swiper/css/pagination";
 const data = [
   {
     avatar: AVATR1,
-    name: "Tina Snow",
+    name: "Responsive Web Design",
     review:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur doloribus aperiam aspernatur quia hic sed alias modi quasi. Corrupti et veritatis minima fuga ipsa non dolorem temporibus explicabo repudiandae sint.",
   },
   {
     avatar: AVATR2,
-    name: "Tina Snow",
+    name: "Introduction to CSS3",
     review:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur doloribus aperiam aspernatur quia hic sed alias modi quasi. Corrupti et veritatis minima fuga ipsa non dolorem temporibus explicabo repudiandae sint.",
   },
   {
     avatar: AVATR3,
-    name: "Tina Snow",
+    name: "Google IT Support",
     review:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur doloribus aperiam aspernatur quia hic sed alias modi quasi. Corrupti et veritatis minima fuga ipsa non dolorem temporibus explicabo repudiandae sint.",
   },
   {
     avatar: AVATR4,
-    name: "Tina Snow",
+    name: "HTML5",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur doloribus aperiam aspernatur quia hic sed alias modi quasi. Corrupti et veritatis minima fuga ipsa non dolorem temporibus explicabo repudiandae sint.",
+  },
+  {
+    avatar: AVATR5,
+    name: "Excel Skills for Business",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur doloribus aperiam aspernatur quia hic sed alias modi quasi. Corrupti et veritatis minima fuga ipsa non dolorem temporibus explicabo repudiandae sint.",
+  },
+  {
+    avatar: AVATR6,
+    name: "Resume Writing",
     review:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur doloribus aperiam aspernatur quia hic sed alias modi quasi. Corrupti et veritatis minima fuga ipsa non dolorem temporibus explicabo repudiandae sint.",
   },
@@ -44,7 +58,7 @@ const data = [
 const Testimonials = () => {
   return (
     <section id="testimonials">
-      <h5>Courses I Have Completed</h5>
+      <h5>External Courses</h5>
       <h2>Certifications</h2>
 
       <Swiper
@@ -61,7 +75,7 @@ const Testimonials = () => {
                 <img src={avatar} alt="Avatar One" />
               </div>
               <h5 className="client__name">{name}</h5>
-              <small className="client__review">{review}</small>
+              
             </SwiperSlide>
           );
         })}
